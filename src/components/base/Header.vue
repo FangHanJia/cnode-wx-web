80033bd3ccb16225155dcbad7d6e80b7f1f423b3
fanghanjia <fanghanjia@interlib.cn>
2022-08-26 16:37:57 +0800

    feat: 【base】-添加基础组件；

---
+++ src/components/base/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <van-sticky class="header-wrap">
+    <van-nav-bar
+      title="标题"
+      left-text="返回"
+      left-arrow
+      @click-left="onClickLeft"
+    />
+  </van-sticky>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const count = ref(0)
+</script>
+
+<style lang="scss" scoped>
+.header-wrap {
+  height: 60px;
+  background: #fff;
+}
+</style>
---
+++ src/components/readme.md
@@ -0,0 +1,4 @@
+# 公共组件
+- base - 基础组件
+- custom - 自定义组件
+- template - 模板组件
